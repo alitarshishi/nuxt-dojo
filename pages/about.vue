988f716f81606f17a765d181f0a4da09dae0c1fd
alitarshishi <ali.tar.22343@gmail.com>
2023-07-22 12:27:11 +0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, blanditiis cupiditate, dolorum ea earum error eum excepturi harum itaque iusto libero maiores nostrum perferendis, quasi qui repellendus rerum sit tenetur?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, blanditiis cupiditate, dolorum ea earum error eum excepturi harum itaque iusto libero maiores nostrum perferendis, quasi qui repellendus rerum sit tenetur?</p>
+    <div>{{data}}</div>
+  </div>
+</template>
+
+<script setup>
+  import {useFetch} from "nuxt/app";
+
+  const { data }= await useFetch('/api/currency/GBP')
+</script>
+
+<style scoped>
+h2{
+  margin-bottom: 20px;
+  font-size: 36px;
+
+}
+p{
+  margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, blanditiis cupiditate, dolorum ea earum error eum excepturi harum itaque iusto libero maiores nostrum perferendis, quasi qui repellendus rerum sit tenetur?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, blanditiis cupiditate, dolorum ea earum error eum excepturi harum itaque iusto libero maiores nostrum perferendis, quasi qui repellendus rerum sit tenetur?</p>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+
+  }
+  p{
+    margin: 20px 0;
+  }
+</style>
